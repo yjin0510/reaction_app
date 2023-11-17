8b2c5e470082ec83934e5d6306362d827f6ee3ac
Yu Jin <YJin@ea.com>
2023-11-16 23:46:53 -0800

    Track reaction time and reset the light to black

--- App.js
+++ App.js
@@ -12,22 +12,36 @@ export default function App() {
   const [frequency, setFrequency] = useState(5);
   // Interval
   const [duration, setDuration] = useState(5);
+  const DEFAULT_COLOR = 'black';
+
+  const [backgroundColor, setBackgroundColor] = useState({ DEFAULT_COLOR });
+  const [index, setIndex] = useState(0);
+  const [cumulativeTimeout, setCumulativeTimeout] = useState(0);
+  const [ts, setTs] = useState(0); // Timestamp
 
   const onCloseSetting = () => {
     setIsModalVisible(false);
   }
 
-  const [backgroundColor, setBackgroundColor] = useState('black');
-  const [index, setIndex] = useState(0);
-  const [cumulativeTimeout, setCumulativeTimeout] = useState(0);
+  const onPress = () => {
+    if (color !== DEFAULT_COLOR) {
+      const reactTime = (Date.now() - ts) / 1000;
+      console.log("React time: " + reactTime + " seconds.");
+    }
+    setBackgroundColor({ DEFAULT_COLOR });
+  }
 
-  useEffect(() => {    
+  useEffect(() => {
     const changeColor = () => {
       const color = plan[index];
       setBackgroundColor(color);
       console.log(color);
-      let timeoutDuration = color === 'black' ?
-      (frequency - 3 + Math.random()) * 1000 : 3000;
+      let timeoutDuration = color === DEFAULT_COLOR ?
+        (frequency - 3 + Math.random()) * 1000 : 3000;
+
+      if (color !== DEFAULT_COLOR) {
+        setTs(Date.now());
+      }
 
       setTimeout(() => {
         if (index < plan.length - 1) {
@@ -43,16 +57,16 @@ export default function App() {
       setIsModalVisible(true);
       return;
     }
-      if (!isModalVisible) {
-        changeColor();
-      } else {
-        setIndex(0);
-      }
+    if (!isModalVisible) {
+      changeColor();
+    } else {
+      setIndex(0);
+    }
   }, [index, plan, frequency, duration]); // Put plan here because it is updated async
 
   return (
     <Pressable style={{ flex: 1 }} onLongPress={() => setIsModalVisible(true)}
-      delayLongPress={2000}>
+      delayLongPress={2000} onPress={onPress}>
       <View style={[styles.color_light, { backgroundColor }]}>
         <AppSetting isVisible={isModalVisible} onCloseSetting={onCloseSetting} setPlan={setPlan} setFreq={setFrequency} setDur={setDuration} />
       </View>
@@ -77,45 +91,3 @@ const styles = StyleSheet.create({
   },
 });
 
-
-// import React, { useState, useEffect } from 'react';
-// import { View, StyleSheet } from 'react-native';
-
-// const App = () => {
-//   const colors = ['red', 'black', 'green', 'black', 'purple', 'orange', 'black'];
-//   const [backgroundColor, setBackgroundColor] = useState('black');
-//   const [index, setIndex] = useState(0);
-
-//   useEffect(() => {
-//     const changeColor = () => {
-//       const color = colors[index];
-//       setBackgroundColor(color);
-
-//       let timeoutDuration = color === 'black' ? (Math.random() * 2) * 1000 : 3000;
-      
-//       setTimeout(() => {
-//         if (index < colors.length - 1) {
-//           setIndex(index + 1);
-//         } else {
-//           setIndex(0);
-//         }
-//       }, timeoutDuration);
-//     };
-
-//     changeColor();
-//   }, [index]);
-
-//   return (
-//     <View style={[styles.container, { backgroundColor }]}></View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-// });
-
-// export default App;
